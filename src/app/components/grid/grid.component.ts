dce744524d3e9c2d9feebbd9c3d8ccbebf48cfea
dapt <diegodapt18@gmail.com>
2025-04-25 18:05:42 +0200

    inserting pagination

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import {  Component, Input, OnInit, inject } from '@angular/core';
 import { Hotel } from '@root/app/types/hotel.type';
 
 @Component({
@@ -9,6 +9,55 @@ import { Hotel } from '@root/app/types/hotel.type';
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
-export class GridComponent {
-  @Input() hotels: Hotel[] = []
+export class GridComponent implements OnInit {
+  @Input() hotels: Hotel[] = [];
+  private generator!: Iterator<number>;
+  public page = 1;
+  public totalPages!: number;
+
+constructor(){}
+
+  ngOnInit(): void {
+    this.startPage()
+  }
+
+  public startPage(): void {
+    this.generator = this.genPage()
+  }
+
+  public getPage() {
+    let val = this.generator.next().value
+    console.log(val)
+    return val
+  }
+
+  public increase() {
+    if(this.page +1 > this.totalPages) return this.startPage()
+    this.page += 1
+    this.startPage()
+  }
+
+  public decrease() {
+    if (this.page - 1 === 0) {
+      this.startPage()
+      return;
+    }
+    this.page -= 1;
+    this.startPage()
+  }
+
+  private *genPage() {
+    let page = 1;
+    let index = 0;
+    let topValue = 9;
+    while (true) {
+      if (index > topValue) {
+        topValue += 10
+        page += 1
+        this.totalPages = page
+      }
+      index += 1
+      yield page;
+    }
+  }
 }
